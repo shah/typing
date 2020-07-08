d61d2712b8eac712e615826e2d1362f7fb64d4be
Shahid N. Shah <shahid@shah.org>
2020-07-08 18:15:45 -0400

    initial commit after extraction from IGS Specification Commons

---
+++ mod.ts
@@ -0,0 +1,54 @@
+import * as inflect from "https://cdn.jsdelivr.net/gh/shah/text-inflect/mod.ts";
+
+// from [this discussion](https://stackoverflow.com/questions/44323441/changing-property-name-in-typescript-mapped-type)
+
+type ValueOf<T> = T[keyof T];
+type KeyValueTupleToObject<T extends [keyof any, any]> = {
+  [K in T[0]]: Extract<T, [K, any]>[1];
+};
+type MapKeys<T, M extends Record<string, string>> =
+  & KeyValueTupleToObject<
+    ValueOf<
+      {
+        [K in RequiredKeys<T>]-?: [K extends keyof M ? M[K] : K, T[K]];
+      }
+    >
+  >
+  & Partial<
+    KeyValueTupleToObject<
+      ValueOf<
+        {
+          [K in OptionalKeys<T>]-?: [K extends keyof M ? M[K] : K, T[K]];
+        }
+      >
+    >
+  > extends infer O ? { [K in keyof O]: O[K] } : never;
+
+type RequiredKeys<T> = { [K in keyof T]-?: {} extends Pick<T, K> ? never : K
+}[keyof T];
+type OptionalKeys<T> = { [K in keyof T]-?: {} extends Pick<T, K> ? K : never
+}[keyof T];
+
+type TransientPropertyMap = {};
+
+export function camelCaseToSnakeCasePropNames<T>(
+  source: T,
+): MapKeys<T, TransientPropertyMap> {
+  const result = {} as MapKeys<T, TransientPropertyMap>;
+  for (var k in source) {
+    // lots of any needed here; hard to convince the type system you're doing the right thing
+    var nk: keyof typeof result = <any> inflect.camelToSnakeLowerCase(k);
+    result[nk] = <any> source[k];
+  }
+  return result;
+}
+
+export function camelCaseToSnakeCasePropNamesMap<T>(
+  source: T,
+): Record<string, string> {
+  var result = {} as Record<string, string>;
+  for (var k in source) {
+    result[k] = <any> inflect.camelToSnakeLowerCase(k);
+  }
+  return result;
+}
---
+++ mod_test.ts
@@ -0,0 +1,29 @@
+import {
+  assertEquals,
+  assert,
+} from "https://deno.land/std@v0.60.0/testing/asserts.ts";
+import * as safe from "./mod.ts";
+
+Deno.test("camelCaseToSnakeCasePropNames", () => {
+  const changed = safe.camelCaseToSnakeCasePropNames(
+    { party: "SYSTEM", partyType: 5, partyIdentifier: 4 },
+  );
+
+  // the above will change each property name to snake case equivalent so it's possbile
+  // that the IDE will complain about types not existing but the test should pass
+  assertEquals("SYSTEM", changed.party);
+  //@ts-ignore
+  assertEquals(5, changed.party_type);
+  //@ts-ignore
+  assertEquals(4, changed.party_identifier);
+});
+
+Deno.test("camelCaseToSnakeCasePropNamesMap", () => {
+  const changed = safe.camelCaseToSnakeCasePropNamesMap(
+    { party: "SYSTEM", partyType: 5, partyIdentifier: 4 },
+  );
+
+  assertEquals("party", changed.party);
+  assertEquals("party_type", changed.partyType);
+  assertEquals("party_identifier", changed.partyIdentifier);
+});
